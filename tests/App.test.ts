56e6ab038ca3c3c77a7f1b28b349ec1c057ee942
Ba Thien Le <bathien.le.pro@gmail.com>
2026-07-26 19:10:44 +0200

    Add title and description to index.html (#6)

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -4,8 +4,9 @@ import { mount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('should render the heading', () => {
     const wrapper = mount(App)
+
     expect(wrapper.text()).toContain('You did it!')
   })
 })
